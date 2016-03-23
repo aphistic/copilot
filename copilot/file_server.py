2b61643162b16881b5cd5c7792457003c0629ec7
Erik Davidson <erik@erikd.org>
2016-03-23 00:00:04 -0500

    Finish terrible web file manager

--- copilot/file_server.py
+++ copilot/file_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 from http.client import HTTPConnection
 from pathlib import Path
 from threading import Thread
@@ -7,6 +7,7 @@ from tkinter.ttk import Frame, Button, Label
 from werkzeug import secure_filename
 import logging
 import os.path
+import shutil
 import socket
 import time
 
@@ -49,20 +50,21 @@ class FileServer(Thread):
 
         @app.route('/', methods=['GET', 'POST'])
         def home():
-            if request.method == 'POST':
-                print('got post')
-                f = request.files['file']
-                if f:
-                    print('got file')
-                    fname = secure_filename(f.filename)
-                    print('fname {}'.format(fname))
-                    fpath = os.path.join(app.config['ROOT_PATH'], fname)
-                    print('saving {} in {}'.format(fpath, os.path.dirname(fpath)))
-                    f.save(fpath)
-            elif request.method == 'DELETE':
-                print('deleting')
-
             web_path = request.args.get('path') or ''
+            if request.method == 'POST':
+                if request.form['action'] == 'create':
+                    f = request.files['file']
+                    if f:
+                        fname = secure_filename(f.filename)
+                        fpath = os.path.join(app.config['ROOT_PATH'], web_path, fname)
+                        f.save(fpath)
+                elif request.form['action'] == 'delete':
+                    fname = request.form['file_name']
+                    fpath = request.form['root_path']
+                    full_path = os.path.join(app.config['ROOT_PATH'], fpath, fname)
+                    if os.path.exists(full_path):
+                        os.remove(full_path)
+
             actual_path = os.path.join(app.config['ROOT_PATH'], web_path)
             path = Path(actual_path)
             dirs = {n.name: os.path.join(web_path, n.name) for n in path.iterdir() if n.is_dir()}
@@ -73,6 +75,21 @@ class FileServer(Thread):
                 parent_dir=os.path.dirname(web_path),
                 dirs=dirs,
                 files=files)
+
+        @app.route("/directory", methods=['POST'])
+        def directory():
+            path = request.form['root_path']
+            dir_name = request.form['dir_name'] or ''
+            change_path = os.path.join(path, dir_name)
+            full_path = os.path.join(app.config['ROOT_PATH'], change_path)
+            if request.form['action'] == "create":
+                if not os.path.exists(full_path):
+                    os.makedirs(full_path)
+            elif request.form['action'] == 'delete':
+                if os.path.exists(full_path):
+                    shutil.rmtree(full_path)
+
+            return redirect('/?path={}'.format(path))
 
         @app.route('/shutdown')
         def shutdown():
