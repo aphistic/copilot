d450525fc6d094caadd11c46223fd37708fac5c3
Erik Davidson <erik@erikd.org>
2016-04-10 13:42:41 -0500

    Bump for v0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'copilot',
     packages = ['copilot'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'Application to manage files on external USB drives',
     author = 'Erik Davidson',
     author_email = 'erik@erikd.org',
     url = 'https://github.com/aphistic/copilot',
-    download_url = 'https://github.com/aphistic/copilot/tarball/0.1.2',
+    download_url = 'https://github.com/aphistic/copilot/tarball/0.1.3',
     keywords = ['raspberrypi', 'raspberry', 'pi', 'filemanager'],
     classifiers = [],
     install_requires = [
