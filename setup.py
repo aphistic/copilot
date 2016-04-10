135f05755ea851cf2e2bc6133f662f33b3c7d6b0
Erik Davidson <erik@erikd.org>
2016-04-10 14:44:14 -0500

    Try again on the manifest file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'copilot',
     packages = ['copilot'],
-    version = '0.1.4',
+    version = '0.1.5',
     description = 'Application to manage files on external USB drives',
     author = 'Erik Davidson',
     author_email = 'erik@erikd.org',
     url = 'https://github.com/aphistic/copilot',
-    download_url = 'https://github.com/aphistic/copilot/tarball/0.1.4',
+    download_url = 'https://github.com/aphistic/copilot/tarball/0.1.5',
     keywords = ['raspberrypi', 'raspberry', 'pi', 'filemanager'],
     classifiers = [],
     install_requires = [
